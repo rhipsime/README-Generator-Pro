30b49e8ba8ef3c48bd825e0fe6230304ce2e438a
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-02-18 21:54:29 +0000

    updates to the readme generate function

--- index.js
+++ index.js
@@ -62,36 +62,70 @@ const questions = [
       name: 'license',
       message: 'Choose the license for your project:',
       choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'Other']
-    }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+      },
+      
   ];  
 
-
-
-// Prompt the user for information
+  // Prompt the user for information
 inquirer.prompt(questions).then((answers) => {
-  // Generate README content based on user input
-  const readmeContent = `
-    # ${answers.title}
-
-    ## Description
-    ${answers.description}
-
-    ## Installation
-    ${answers.installation}
-
-    ## Usage
-    ${answers.usage}
-
-    ## License
-    ${answers.license}
-  `;
-
-  // Write README content to file
-  fs.writeFile('README.md', readmeContent, (err) => {
-    if (err) {
-      console.error('Error writing README file:', err);
-    } else {
-      console.log('README file generated successfully!');
-    }
+    // Generate README content based on user input
+    const readmeContent = generateREADME(answers);
+  
+    // Write README content to file
+    fs.writeFile('README.md', readmeContent, (err) => {
+      if (err) {
+        console.error('Error writing README file:', err);
+      } else {
+        console.log('README file generated successfully!');
+      }
+    });
   });
-});
+  
+  function generateREADME(answers) {
+    // GitHub profile link
+    const githubLink = answers.github ? `[${answers.github}](https://github.com/${answers.github})` : 'None';
+  
+    return `
+  # ${answers.title}
+  
+  ## Description
+  ${answers.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${answers.installation}
+  
+  ## Usage
+  ${answers.usage}
+  
+  ## Credits
+  ${answers.credits}
+  
+  ## License
+  This project is licensed under the ${answers.license} License.
+  
+  ## Questions
+  - [GitHub](${answers.github ? 'https://github.com/' + answers.github : '#'})  
+  - Email: ${answers.email ? answers.email : 'Not provided'}
+  `;
+  }
+  
+  
+
+ 
